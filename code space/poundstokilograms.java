8be83a418897131558baafc8df28590b074e2867
Tapasweereddysandadi <tapasweereddysandadi@gmail.com>
2026-07-11 16:03:09 +0530

    Add files via upload

---
+++ code space/poundstokilograms.java
@@ -0,0 +1,10 @@
+public class poundstokilograms {
+     public static void main(String args[]) {
+         double pounds=30;
+         double kilograms=pounds * 0.45359237;
+         System.out.println("Pounds "+pounds);
+         System.out.println("Kilograms "+kilograms);
+    }
+}
+
+         
