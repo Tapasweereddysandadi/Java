2477aa68d7eeb60a8bc570f6708d5db1e6bfc937
Tapasweereddysandadi <tapasweereddysandadi@gmail.com>
2026-07-11 18:41:30 +0530

    Add files via upload

---
+++ code space/Tue.java
@@ -0,0 +1,13 @@
+public class Tue {
+    public static void main(String args[]) {
+       int intval=10;
+       float floatval=10;
+       double doubleval=10;
+       float f=5f/2f;
+       System.out.println("int val "+intval);
+       System.out.println("float val "+floatval);
+       System.out.println("double val "+doubleval);
+       System.out.println(f);
+       
+   }
+}
