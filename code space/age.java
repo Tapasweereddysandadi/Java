2fc0af1d52de922704f207aaf091706bd1761265
Tapasweereddysandadi <tapasweereddysandadi@gmail.com>
2026-07-13 19:29:02 +0530

    Add files via upload

---
+++ code space/age.java
@@ -0,0 +1,10 @@
+public class age {
+   public static void main(String args[]) {
+        int age=9;
+        if (age>=18) {
+        System.out.println("Eligible to Vote");
+     } else {
+        System.out.println("Not Eligible to vote ");
+     }
+   }
+}
