41a62bd2705553dbc6c2267ae57e5673ceabfc30
Tapasweereddysandadi <tapasweereddysandadi@gmail.com>
2026-07-28 13:50:34 +0530

    Add files via upload

---
+++ code space/Wed.java
@@ -0,0 +1,15 @@
+public class Wed {
+    public static void main(String args[]) {
+        int intval=10;
+       float floatval=10;
+       double doubleval=10;
+       float f=5f/3f;
+       double d=5f/3f;
+       System.out.println("int val "+intval);
+       System.out.println("float val "+floatval);
+       System.out.println("double val "+doubleval);
+       System.out.println(f);
+       System.out.println(d);
+       System.out.println("Float range is " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+  }
+}
---
+++ code space/def.java
@@ -0,0 +1,14 @@
+public class def {
+    public static void main(String args[]) {
+        int age = 19, rollNo = 8;
+        age=age+2;
+        int minVal = Integer.MIN_VALUE;
+        System.out.println(minVal);
+        int maxVal = Integer.MAX_VALUE;
+        System.out.println(maxVal);
+        int salary=maxVal+1;
+        System.out.println("Roll No: " + rollNo + " Age: " + age);
+        long i=214748368L;
+        System.out.println(i);
+    }
+}
