17d8bcc7b6977d40c965e2fc097cee9bdfcad49e
Tapasweereddysandadi <tapasweereddysandadi@gmail.com>
2026-08-18 15:13:27 +0530

    Add files via upload

---
+++ code space/Student.java
@@ -0,0 +1,20 @@
+public class Student {
+     String name;
+     String rollno;
+     public static void main(String args[]) {
+         Student S1=new Student();
+         S1.name= "Tapaswee";
+         S1.rollno="H9";
+         Student S2=new Student();
+         S2.name= "Sirimalli";
+         S2.rollno="H2";
+         S1.display();
+         S2.display();
+   }
+    void display() {
+          System.out.println(name);
+          System.out.println(rollno);
+          System.out.println("------");
+     }
+}
+ 
