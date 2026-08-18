43c83182a83c05ac5c0aad3a21a05575b0ae81c2
Tapasweereddysandadi <tapasweereddysandadi@gmail.com>
2026-08-18 15:05:11 +0530

    Add files via upload

---
+++ code space/array.java
@@ -0,0 +1,9 @@
+class array {
+    public static void main(String args[]) {
+        String name[]=new String[]{"Tapaswee","Sirimalli"};
+        System.out.println("Size of array : " +name.length);
+        for (int i=0;i<name.length;i++) {
+               System.out.println(name[i]);
+        }
+    }
+}
